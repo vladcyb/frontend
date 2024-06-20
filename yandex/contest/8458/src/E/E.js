60a4cefcdf78a05a7ecbaf330468a07195890b9b
Vlad Tsibizov <vladtsibizov@yandex.ru>
2024-06-20 23:51:23 +0300

    Обновил решение yandex/contest/8458/E

--- yandex/contest/8458/src/E/E.js
+++ yandex/contest/8458/src/E/E.js
@@ -20,21 +20,18 @@ function areAnagrams(firstArr, secondArr) {
   const { length } = firstArr
 
   for (let i = 0; i < length; i++) {
-    const count = firstMap.get(firstArr[i])
-    firstMap.set(firstArr[i], (count ?? 0) + 1)
+    const count1 = firstMap.get(firstArr[i])
+    firstMap.set(firstArr[i], (count1 ?? 0) + 1)
+    const count2 = secondMap.get(secondArr[i])
+    secondMap.set(secondArr[i], (count2 ?? 0) + 1)
   }
 
-  for (let i = 0; i < length; i++) {
-    const count = secondMap.get(secondArr[i])
-    secondMap.set(secondArr[i], (count ?? 0) + 1)
-  }
-
-  for (let i = 0; i < length; i++) {
-    const letter = firstArr[i]
-    if (firstMap.get(letter) !== secondMap.get(letter)) {
+  for (const [key] of firstMap) {
+    if (firstMap.get(key) !== secondMap.get(key)) {
       return false
     }
   }
+
   return true
 }
 
